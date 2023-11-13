3c79419ced2311cb5dce5e7a599acf8e2c990196
MarcosF23 <142623071+MarcosF23@users.noreply.github.com>
2023-11-13 19:39:22 -0300

    Add files via upload

---
+++ Practica/PracticaJ.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuOptions = document.getElementById('menuOptions');
+    if (menuOptions.style.display === 'block') {
+        menuOptions.style.display = 'none';
+    } else {
+        menuOptions.style.display = 'block';
+    }
+}
